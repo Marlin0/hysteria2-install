c9a4cf80b4942b71978246f2f58b1146419158cc
Marlin <34276308+Marlin0@users.noreply.github.com>
2026-03-14 12:32:00 +0800

    Update hysteria.sh

--- hysteria2-install-main/hy2/hysteria.sh
+++ hysteria2-install-main/hy2/hysteria.sh
@@ -88,7 +88,7 @@ inst_cert(){
             green "已输入的域名：$domain" && sleep 1
             green "服务器IP：$ip" && sleep 1
             # domainIP=$(curl -sm8 ipget.net/?ip=${domain})
-            domainIP=$(curl -sm8 http://ip-api.com/)
+            domainIP=$(curl -sm8 "http://ip-api.com")
             green "域名IP：$domainIP" && sleep 1
             if [[ $domainIP == $ip ]]; then
                 ${PACKAGE_INSTALL[int]} curl wget sudo socat openssl
